4052f640cf9eb713397676e281c612eeb775f3ac
Woile <santiwilly@gmail.com>
2015-07-09 16:57:50 -0300

    Fixed issue in module index

--- templates/module/template/index.js
+++ templates/module/template/index.js
@@ -1,4 +1,10 @@
 /**
- * This should export a view, which should be used inside
- * a controller.
+ * This should export a function, which should return an instanciated
+ * view (to avoid problems with require), and in your controlle
+ * the method returned should be called.
  */
+
+module.exports = function () {
+    'use strict';
+
+};
